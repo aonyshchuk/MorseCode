400396a3eb1c3ea09a870d44fffba598dd740d5f
aonyshchuk <alinaonyshchuk7@gmail.com>
2023-09-23 12:11:26 +0300

    Add project files.

---
+++ MorseCodeTranslator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    public class MorseCodeTranslator
+    {
+        public class MorseToTextTranslator
+        {
+            public Dictionary<string, char> morseCodeDictionary;
+
+            public MorseToTextTranslator()
+            {
+                InitializeMorseCodeDictionary();
+            }
+
+            public void InitializeMorseCodeDictionary()
+            {
+                morseCodeDictionary = new Dictionary<string, char>
+            {
+                {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'}, {".", 'E'},
+                {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'}, {"..", 'I'}, {".---", 'J'},
+                {"-.-", 'K'}, {".-..", 'L'}, {"--", 'M'}, {"-.", 'N'}, {"---", 'O'},
+                {".--.", 'P'}, {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'},
+                {"..-", 'U'}, {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'}, {"-.--", 'Y'},
+                {"--..", 'Z'},
+                {"-----", '0'}, {".----", '1'}, {"..---", '2'}, {"...--", '3'}, {"....-", '4'},
+                {".....", '5'}, {"-....", '6'}, {"--...", '7'}, {"---..", '8'}, {"----.", '9'},
+                {" ", ' '} 
+            };
+            }
+
+            public string TranslateToText(string morseCode)
+            {
+                string[] words = morseCode.Split('/');
+                string text = "";
+
+                foreach (string word in words)
+                {
+                    string[] letters = word.Trim().Split(' ');
+                    foreach (string letter in letters)
+                    {
+                        if (morseCodeDictionary.ContainsKey(letter))
+                        {
+                            text += morseCodeDictionary[letter];
+                        }
+                        else
+                        {
+                            text += " ";
+                        }
+                    }
+                    text += " "; 
+                }
+
+                return text.Trim(); 
+            }
+        }
+    }
+}
+
+namespace MorseCode
+{
+    public class TextToMorseTranslator
+    {
+        public Dictionary<char, string> morseCodeDictionary;
+
+        public TextToMorseTranslator()
+        {
+            InitializeMorseCodeDictionary();
+        }
+
+        public void InitializeMorseCodeDictionary()
+        {
+            morseCodeDictionary = new Dictionary<char, string>
+            {
+                {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+                {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+                {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+                {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+                {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+                {'Z', "--.."},
+                {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"},
+                {'5', "....."}, {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."},
+                {' ', " "}, 
+            };
+        }
+
+        public string TranslateToMorse(string text)
+        {
+            text = text.ToUpper(); 
+            string morseCode = "";
+
+            foreach (char character in text)
+            {
+                if (morseCodeDictionary.ContainsKey(character))
+                {
+                    morseCode += morseCodeDictionary[character] + " ";
+                }
+                else
+                {
+                    morseCode += " "; 
+                }
+            }
+
+            return morseCode.Trim(); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using static MorseCode.MorseCodeTranslator;
+
+namespace MorseCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+            TextToMorseTranslator textToMorseTranslator = new TextToMorseTranslator();
+            MorseToTextTranslator morseToTextTranslator = new MorseToTextTranslator();
+
+            while (true)
+            {
+                Console.WriteLine("Choose the option:");
+                Console.WriteLine("1. Text to Morse Code");
+                Console.WriteLine("2. Morse Code to Text");
+                Console.WriteLine("3. Exit");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("Enter text for traslation: ");
+                            string inputText = Console.ReadLine();
+                            string morseCode = textToMorseTranslator.TranslateToMorse(inputText);
+                            Console.WriteLine($"The result is: {morseCode}");
+                            break;
+                        case 2:
+                            Console.Write("Enter the Morse code for traslation: ");
+                            string inputMorseCode = Console.ReadLine();
+                            string text = morseToTextTranslator.TranslateToText(inputMorseCode);
+                            Console.WriteLine($"The result is: {text}");
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Error!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+    
